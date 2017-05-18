c26abe968ee5f8c6f9b368deb8de6d1122d5c1fa
vicmagv <vicmagv@gmail.com>
2017-05-18 17:31:14 +0200

    new endpoint

--- api/server.py
+++ api/server.py
@@ -1,6 +1,5 @@
 from flask import Flask, abort, request, jsonify
 from flask_cors import CORS, cross_origin
-from pymongo import MongoClient
 import pymongo
 import datetime
 
@@ -15,10 +14,10 @@ MONGO_PORT = 27017
 class MongoDBRepository:
 
     def __init__(self):
-        self.db = MongoClient(MONGO_HOST, MONGO_PORT).sello
+        self.db = pymongo.MongoClient(MONGO_HOST, MONGO_PORT).sello
 
-    def findAllMeasures(self):
-        return [x for x in self.db.measures.find({}, {"_id": 0}).sort("ocurredOn", pymongo.DESCENDING)]
+    def findAllMeasures(self, limit):
+        return [x for x in self.db.measures.find({}, {"_id": 0}).sort("ocurredOn", pymongo.DESCENDING).limit(int(limit))]
 
     def createMeasures(self, temp, hum):
         self.db.measures.insert_one({"temp": temp, "hum": hum, "ocurredOn": datetime.datetime.now()})
@@ -38,12 +37,10 @@ class MongoDBRepository:
         self.db.letter_introduced.insert_one({"ocurredOn": datetime.datetime.now()})
         return True
     
-    def isInboxEmpty(self):
-        letter = [x for x in self.db.letter_introduced.find({}, {"_id": 0}).sort("ocurredOn", pymongo.DESCENDING).limit(1)][0]
+    def mailbox(self):
         door = [x for x in self.db.door_opened.find({}, {"_id": 0}).sort("ocurredOn", pymongo.DESCENDING).limit(1)][0]
-        if letter['ocurredOn'] > door['ocurredOn']:
-            return False
-        return True
+        count = self.db.letter_introduced.count({"ocurredOn": {"$gt": door['ocurredOn']}})
+        return count
 
 def check_api_key():
     if request.headers.get('X-Api-Key') != API_KEY:
@@ -76,9 +73,13 @@ def open_door():
 def measures():
     check_api_key()
     mongo_repo = MongoDBRepository()
-
     if request.method == 'GET':
-        return jsonify(mongo_repo.findAllMeasures())
+        if 'limit' in request.args:
+            limit = request.args.get('limit')
+        else:
+            limit = 0
+
+        return jsonify(mongo_repo.findAllMeasures(int(limit)))
 
     temp = request.values['temp']
     hum = request.values['hum']
@@ -86,10 +87,8 @@ def measures():
     mongo_repo.createMeasures(temp, hum)
     return "New temperature measure registered successfully"
 
-@app.route("/empty", methods=['GET'])
-def empty():
+@app.route("/mailbox", methods=['GET'])
+def mailbox():
     check_api_key()
     mongo_repo = MongoDBRepository()
-    return jsonify({"empty": mongo_repo.isInboxEmpty()})
-
-
+    return jsonify({"count": mongo_repo.mailbox()})
