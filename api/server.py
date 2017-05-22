5771131eab7f12b3a8bab36edcd28575ecef2bd4
vicmagv <vicmagv@gmail.com>
2017-05-22 19:02:48 +0200

    redis and more things

--- api/server.py
+++ api/server.py
@@ -2,6 +2,7 @@ from flask import Flask, abort, request, jsonify
 from flask_cors import CORS, cross_origin
 import pymongo
 import datetime
+import redis
 
 app = Flask(__name__)
 app.debug = True
@@ -10,6 +11,31 @@ CORS(app)
 API_KEY = "test api key"
 MONGO_HOST = "mongo"
 MONGO_PORT = 27017
+REDIS_HOST = "redis"
+REDIS_PORT = 6379
+
+class RedisRepository:
+    def __init__(self):
+        self.r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    def front_door_open(self):
+        self.r.set('front', "open")
+
+    def front_door_close(self):
+        self.r.set('front', "close")
+
+    def top_door_open(self):
+        self.r.set('top', "open")
+
+    def top_door_close(self):
+        self.r.set('top', "close")
+
+    def front_door(self):
+        return self.r.get('front').decode("utf-8") 
+
+    def top_door(self):
+        return self.r.get('top').decode("utf-8") 
+
 
 class MongoDBRepository:
 
@@ -57,18 +83,6 @@ def introduce_letter():
     mongo_repo.createLetterEvent()
     return "LETTER_INTRODUCED_EVENT registered successfully"
 
-@app.route("/door", methods=['POST', 'GET'])
-def open_door():
-    check_api_key()
-    mongo_repo = MongoDBRepository()
-
-    if request.method == 'GET':
-        return jsonify(mongo_repo.findAllDoorEvents())
-    
-    mongo_repo.createDoorEvent()
-
-    return "DOOR_OPENED_EVENT registered successfully"
-
 @app.route("/measures", methods=['POST', 'GET'])
 def measures():
     check_api_key()
@@ -91,4 +105,46 @@ def measures():
 def mailbox():
     check_api_key()
     mongo_repo = MongoDBRepository()
-    return jsonify({"count": mongo_repo.mailbox()})
+    redis_repo = RedisRepository()
+    return jsonify({
+                "count": mongo_repo.mailbox(), 
+                'front': redis_repo.front_door(), 
+                'top': redis_repo.top_door()
+                })
+
+@app.route("/door/close", methods=['POST'])
+def close_door():
+    check_api_key()
+    redis_repo = RedisRepository()
+    redis_repo.front_door_close()
+    return "DOOR_CLOSED_EVENT registered successfully"
+
+@app.route("/door", methods=['POST', 'GET'])
+def open_door():
+    check_api_key()
+    mongo_repo = MongoDBRepository()
+    redis_repo = RedisRepository()
+
+    if request.method == 'GET':
+        return jsonify(mongo_repo.findAllDoorEvents())
+    
+    mongo_repo.createDoorEvent()
+    redis_repo.front_door_open()
+
+
+    return "DOOR_OPENED_EVENT registered successfully"
+
+@app.route("/top/close", methods=['POST'])
+def close_top():
+    check_api_key()
+    redis_repo = RedisRepository()
+    redis_repo.top_door_close()
+    return "TOP_CLOSED_EVENT registered successfully"
+
+@app.route("/top", methods=['POST'])
+def open_top():
+    check_api_key()
+    redis_repo = RedisRepository()
+    redis_repo.top_door_open()
+    return "TOP_OPENED_EVENT registered successfully"
+
